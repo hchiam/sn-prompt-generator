7971542930869e9ca0d7c57455ad5090698bfed6
hchiam <hchiam@users.noreply.github.com>
2020-01-09 15:50:36 -0500

    fix #32

--- service-worker.js
+++ service-worker.js
@@ -1,28 +1,43 @@
-var CACHE_NAME = 'version_01';
+var CACHE_NAME = 'version_02';
 
-var URLS = [
+var appShellURLs = [
   'index.html',
-  'offline-page.html',
   'minified-script.js',
-  'style.css',
+  'offline-page.html',
+  'minified-style.css',
 ];
 
-self.addEventListener('install', function (e) {
-  e.waitUntil(caches.open(CACHE_NAME)
+self.addEventListener('install', function (event) {
+  event.waitUntil(caches.open(CACHE_NAME)
     .then(function (cache) {
       console.log('Service worker installing.')
-      return cache.addAll(URLS);
+      return cache.addAll(appShellURLs);
     })
   );
 });
 
-self.addEventListener('fetch', function (e) {
-  if (e.request.mode === 'navigate') {
-    e.respondWith(fetch(e.request)
-      .catch(function (err) {
+self.addEventListener('fetch', function (event) {
+  var url = new URL(event.request.url);
+  if (appShellURLs.indexOf(url.pathname) !== -1) {
+    event.respondWith(caches.match(event.request)
+      .then(function (response) {
+        if (!response) {
+          throw new Error(event.request + ' not found in cache');
+        }
+        console.log('Service worker working even though you are offline.');
+        return response;
+      })
+      .catch(function (error) {
+        fetch(event.request);
+      })
+    );
+  } else if (event.request.mode === 'navigate') {
+    event.respondWith(fetch(event.request)
+      .catch(function (error) {
         return caches.open(CACHE_NAME)
           .then(function (cache) {
-            console.log('Service worker working even though you are offline.')
+            console.log('Service worker working even though you are offline.');
+            // return cache.matchAll(URLS);
             return cache.match('offline-page.html');
           });
       })
